ca8475ac23ab6e8e4d450102f5af0fe6e861dc3a
jaewook3203 <jae3wook0707@naver.com>
2021-09-02 11:01:05 +0900

    초기 시작 상태

---
+++ main_cm0p.c
@@ -0,0 +1,13 @@
+#include "project.h"
+
+int main(void)
+{
+    __enable_irq(); 
+    mUART_Start();
+    for(;;)
+    {
+        mUART_PutString("Hello world\n");
+        CyDelay(1000);
+    }
+}
+
